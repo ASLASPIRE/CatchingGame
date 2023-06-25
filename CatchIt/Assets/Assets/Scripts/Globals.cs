e1db26e777eaa636ae4d5fa667489e32116808a4
Saxophone239 <70723899+Saxophone239@users.noreply.github.com>
2023-06-24 21:53:03 -0500

    feat: add menu screen

--- CatchIt/Assets/Assets/Scripts/Globals.cs
+++ CatchIt/Assets/Assets/Scripts/Globals.cs
@@ -2,15 +2,20 @@ using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static MenuScreenManager;
 
 public class Globals : MonoBehaviour
 {
     public static TextAsset jsonFile;
-    public static string vocabSet = "PartsOfTheCell";
+    public static string vocabSet = "PartsOfTheCell"; //to be deprecated
     public static int level = 1; 
     public static float fallingSpeed = 0.2f;
     public static float spawnRate = 0.9f;
     public static int incrementValue = 150;
     public static List<PlayerLeaderboardEntry> leaderboardEntries = new List<PlayerLeaderboardEntry>();
     public static  bool tutorial = false;
+    public static Difficulty difficulty = Difficulty.Easy;
+    public static Vocab vocabList = Vocab.PartsOfTheCell;
+
+
 }
